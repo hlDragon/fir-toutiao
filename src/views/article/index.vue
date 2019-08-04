1fbdc2c5b249fa7bcef5c9e5ce6718e35bce5e9e
fishlay <897635602@qq.com>
2019-08-04 19:28:05 +0800

    封装axios

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -3,7 +3,13 @@
 </template>
 
 <script>
-export default {}
+export default {
+  created () {
+    this.$http.get('articles').then(res => {
+      console.log(res.data)
+    })
+  }
+}
 </script>
 
 <style scoped lang='less'></style>
