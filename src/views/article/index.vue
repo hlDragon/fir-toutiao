7e3ed73a0eb20543d7131aba6c8dfa66f95c89dd
fishlay <897635602@qq.com>
2019-08-07 19:52:21 +0800

    筛选结果布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,50 +1,73 @@
 <template>
-  <el-card class="box-card">
-    <div slot="header">
-      <my-bread>内容管理</my-bread>
-    </div>
-    <el-form
-      label-width="80px"
-      size="small"
-    >
-      <el-form-item label="状态：">
-        <el-radio-group v-model="reqParams.status">
-          <el-radio :label="null">全部</el-radio>
-          <el-radio :label="0">草稿</el-radio>
-          <el-radio :label="1">待审核</el-radio>
-          <el-radio :label="2">审核通过</el-radio>
-          <el-radio :label="3">审核失败</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="频道：">
-        <el-select
-          v-model="reqParams.channel_id"
-          placeholder="请选择"
-        >
-          <el-option
-            v-for="item in channelOptions"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
+  <div>
+    <el-card class="box-card">
+      <div slot="header">
+        <my-bread>内容管理</my-bread>
+      </div>
+      <el-form
+        label-width="80px"
+        size="small"
+      >
+        <el-form-item label="状态：">
+          <el-radio-group v-model="reqParams.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道：">
+          <el-select
+            v-model="reqParams.channel_id"
+            placeholder="请选择"
           >
-          </el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="日期：">
-        <el-date-picker
-          v-model="dataArr"
-          type="daterange"
-          range-separator="至"
-          start-placeholder="开始日期"
-          end-placeholder="结束日期"
-        >
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary">筛选</el-button>
-      </el-form-item>
-    </el-form>
-  </el-card>
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期：">
+          <el-date-picker
+            v-model="dataArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          >
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card>
+      <div slot="header">
+        根据筛选条件共查询到0条结果：
+      </div>
+    </el-card>
+    <el-table :data="articles">
+      <el-table-column
+        prop="date"
+        label="封面"
+        width="180"
+      >
+      </el-table-column>
+    </el-table>
+    <div style="text-align:center;margin-top:30px">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000"
+      >
+      </el-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -56,10 +79,15 @@ export default {
         channel_id: null
       },
       channelOptions: [],
-      dataArr: []
+      dataArr: [],
+      articles: []
     }
   }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.el-card {
+  margin-bottom: 20px;
+}
+</style>
