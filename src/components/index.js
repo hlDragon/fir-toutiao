8b914626ad162c86d9c4b186ce018bcf3b24b115
fishlay <897635602@qq.com>
2019-08-10 22:08:58 +0800

    发布及修改文章完成

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import MyBread from '@/components/mybread'
 import myChannel from '@/components/myChannel'
+import MyImage from '@/components/my-image'
 
 export default {
   install (Vue) {
     Vue.component(MyBread.name, MyBread)
     Vue.component(myChannel.name, myChannel)
+    Vue.component(MyImage.name, MyImage)
   }
 }
